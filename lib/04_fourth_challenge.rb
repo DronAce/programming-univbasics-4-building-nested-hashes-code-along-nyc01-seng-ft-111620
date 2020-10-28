005e777e9fd31d11f0492917bf3d8134242c69fd
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-28 15:25:59 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        first_friend: { 
+        first_friend: {
           name: "Benvolio",
           age: "17",
           attitude: "worried"
@@ -19,7 +19,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        first_friend: { 
+        first_friend: {
           name:
           age:
           attitude:
