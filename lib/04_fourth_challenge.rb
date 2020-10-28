12194ed2a065d7b31857bef2ca9db0a53115f297
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-28 15:21:46 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,8 +6,11 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        name:
+        age:
+        attitude:
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -18,6 +21,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
