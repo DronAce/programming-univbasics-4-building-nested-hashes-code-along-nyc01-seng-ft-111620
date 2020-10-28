b3b6a20c45b112b223970505df58d27aac285f0f
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-28 15:17:06 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -22,8 +22,8 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        name: ,
-        age:
+        name: "Lord Capulet",
+        age: "50"
         },
       :matriarch => {
         #your key/value pairs here
