85c4fbc13b2acabcdf01146c5f316693579e469f
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-28 15:17:41 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -32,7 +32,7 @@ def third_challenge
         },
       :heroine => {
         #your key/value pairs here
-        name: ,
+        name: "Juliet",
         age:
         status: "alive"
         },
